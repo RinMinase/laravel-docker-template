e885db89bdd693094107d5b0ba01305962b8bc18
Rin Minase <rin.black.raison.detre@gmail.com>
2026-03-25 17:46:54 +0800

    Fixed lacking files and functions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,21 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Providers\AppServiceProvider;
+
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
     web: __DIR__ . '/routes.php',
     commands: __DIR__ . '/console.php',
     health: '/up',
   )
+  ->withProviders([
+    AppServiceProvider::class,
+  ])
   ->withMiddleware(function (Middleware $middleware): void {
     //
   })
   ->withExceptions(function (Exceptions $exceptions): void {
     //
-  })->create();
+  })
+  ->create();

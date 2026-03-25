dd11a81c88a9a582e37549acea5899f2df6cc22b
Rin Minase <rin.black.raison.detre@gmail.com>
2026-03-25 17:47:43 +0800

    Moved blade files to application

---
+++ config/view.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  /* View / Blade Storage Paths */
+  'paths' => [realpath(base_path('app/Views'))],
+
+  /* Compiled View Path */
+  'compiled' => realpath(storage_path('framework/views')),
+];
